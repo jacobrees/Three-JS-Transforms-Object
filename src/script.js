3511e6f51b008f0019506caa9386ee51b61ad5ea
jacobrees <jacob.rees97@yahoo.com>
2023-04-05 17:25:42 +0100

    Tidy up code

--- src/script.js
+++ src/script.js
@@ -12,35 +12,33 @@ const scene = new THREE.Scene();
  */
 
 const group = new THREE.Group();
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y =1
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
 scene.add(group);
 
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
 
 group.add(cube1);
 
 const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 );
 
 group.add(cube2);
-cube2.position.x = -2
+cube2.position.x = -2;
 
 const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
 );
 
 group.add(cube3);
-cube3.position.x = 2
-
-
+cube3.position.x = 2;
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
